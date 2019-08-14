31aaddf10b9957832df547f3397b3e845af7b488
AC05 <oderocharlesouma@gmail.com>
2019-08-14 17:16:09 +0300

    fourth commit

--- app/src/main/java/www/charles/gochat/com/UserProfileSettingsActivity.java
+++ app/src/main/java/www/charles/gochat/com/UserProfileSettingsActivity.java
@@ -4,26 +4,35 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
 import com.theartofdev.edmodo.cropper.CropImage;
 import com.theartofdev.edmodo.cropper.CropImageView;
 
@@ -39,10 +48,15 @@ public class UserProfileSettingsActivity extends AppCompatActivity {
     private FloatingActionButton sttngsFavCamera;
 
     private DatabaseReference getUserDataReference;
+    private ProgressDialog loadingBar;
 
     private FirebaseAuth mAuth;
     private static final int PICK_IMAGE_REQUEST = 1;
+    private static final int TAKE_PIC_REQUEST = 2;
     private StorageReference storeUserProfileImageRef;
+    private FirebaseStorage firebaseStorage;
+    private Uri imageUri;
+    private String  image;
 
 
     @Override
@@ -50,6 +64,8 @@ public class UserProfileSettingsActivity extends AppCompatActivity {
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile_settings);
+
+        loadingBar = new ProgressDialog(this);
 
         /**
          * creating a reference to the firebase auth
@@ -59,6 +75,7 @@ public class UserProfileSettingsActivity extends AppCompatActivity {
          */
         mAuth = FirebaseAuth.getInstance();
         String onlineCurrentUser = mAuth.getCurrentUser().getUid();
+        firebaseStorage = FirebaseStorage.getInstance();
 
         /**
          * Creating a reference to the database
@@ -93,12 +110,17 @@ public class UserProfileSettingsActivity extends AppCompatActivity {
                  * this object allow us to get all the user data from the database
                  * String username = dataSnapshot.child("user_name").getValue().toString();
                  *                 String userStatus = dataSnapshot.child("user_status").getValue().toString();
+                 *
                  */
 
 
                 /**
-                 * Setting the data in the app
+                 * Setting the data in the
+                 *  String image = dataSnapshot.child("user_image").getValue().toString();
+                 *                 Picasso.get().load(image).into(sttngsDisplayUserProfileImage);
                  */
+
+
 
             }
 
@@ -108,6 +130,37 @@ public class UserProfileSettingsActivity extends AppCompatActivity {
 
             }
         });
+        getUserDataReference.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded( DataSnapshot dataSnapshot,  String image)
+            {
+
+
+
+            }
+
+            @Override
+            public void onChildChanged( DataSnapshot dataSnapshot,  String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+
         /**
          * This code is for enabling the user to select a photo from the gallery
          * then set the photo as a profile photo
@@ -124,7 +177,21 @@ public class UserProfileSettingsActivity extends AppCompatActivity {
 
              }
          });
+         //Enabling the user ta take a photo using the camera
+         sttngsFavCamera.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+                 pictureTakerAction();
 
+             }
+         });
+
+    }
+
+    private void pictureTakerAction() {
+
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(cameraIntent, TAKE_PIC_REQUEST);
     }
 
     @Override
@@ -137,52 +204,108 @@ public class UserProfileSettingsActivity extends AppCompatActivity {
             {
                 if (data!=null)
                 {
-                    Uri imageUri = data.getData();
-                    /**
-                     * Setting the crop feature to the image
-                     */
-                    CropImage.activity()
-                             .setGuidelines(CropImageView.Guidelines.ON)
-                             .setAspectRatio(1,1)
-                             .start(this);
+                    imageUri = data.getData();
+                     uploadUserProfile();
 
                 }
-                if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE)
+                else {
+                    if (resultCode == RESULT_CANCELED)
+                    {
+                        Toast.makeText(this, "No image selected", Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+
+            }
+        }
+
+        //SETTING UP THE CAMERA
+        if (requestCode == TAKE_PIC_REQUEST)
+        {
+            if(resultCode == RESULT_OK)
+            {
+                if (data != null)
                 {
-                    CropImage.ActivityResult result = CropImage.getActivityResult(data);
-                    if (resultCode == RESULT_OK)
-                    {
-                        Uri resultUri = result.getUri();
-                        /**
-                         * Creating a reference to enble the user to send the image to the database
-                         * if the image has been cropped successfully, it is stored in the resultUri varriable
-                         * give the photo a storage name using the unique user id
-                         */
-                        String userId = mAuth.getCurrentUser().getUid();
-                        StorageReference filePath = storeUserProfileImageRef.child(userId + "jpn");
-                        //putting the croped photo into our firebase dtabase
-                        filePath.putFile(resultUri).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>()
-                        {
-                            @Override
-                            public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task)
-                            {//Checking if the thoto has been successfully sent to the firebase database
-                                if (task.isSuccessful())
-                                {
-                                    Toast.makeText(UserProfileSettingsActivity.this, "Uploading profile to the server...", Toast.LENGTH_SHORT).show();
-                                }
-                                else
-                                {
-                                    Toast.makeText(UserProfileSettingsActivity.this, "Error occurred. Please try again", Toast.LENGTH_SHORT).show();
-                                }
-                            }
-                        });
-                    }
-                    else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE)
-                    {
-                        Exception errors = result.getError();
-                    }
+                    Bitmap capturedImage = (Bitmap)data.getExtras().get("data");
+                    sttngsDisplayUserProfileImage.setImageBitmap(capturedImage);
+
+                }
+                else if (resultCode == RESULT_CANCELED)
+                {
+                    Toast.makeText(this, "Cancelled", Toast.LENGTH_SHORT).show();
                 }
             }
         }
+
+    }
+
+    private void uploadUserProfile() {
+
+        if (imageUri != null)
+        {
+            loadingBar.setTitle("Uploading Image");
+            loadingBar.show();
+            storeUserProfileImageRef.putFile(imageUri)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>()
+                    {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot)
+                        {
+                            Handler handler = new Handler();
+                            handler.postDelayed(new Runnable()
+                            {
+                                @Override
+                                public void run()
+                                {
+
+                                    loadingBar.setProgress(0);
+
+                                }
+                            }, 5000);
+                            Toast.makeText(UserProfileSettingsActivity.this, "Image uploaded successfully",
+                                    Toast.LENGTH_SHORT).show();
+                            Picasso.get().load(image).into(sttngsDisplayUserProfileImage);
+
+
+                            loadingBar.dismiss();
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener()
+                    {
+                        @Override
+                        public void onFailure(@NonNull Exception e)
+                        {
+                            loadingBar.dismiss();
+                            Toast.makeText(UserProfileSettingsActivity.this, "Failed to upload image" + e.getMessage(),
+                                            Toast.LENGTH_SHORT).show();
+
+                        }
+                    })
+                    .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>()
+                    {
+                        @Override
+                        public void onProgress(UploadTask.TaskSnapshot taskSnapshot)
+                        {
+
+                            double uploadingUserImage = (100.0 *  taskSnapshot.getBytesTransferred() / taskSnapshot.getTotalByteCount());
+                            loadingBar.setMessage("Uploaded" + (int) uploadingUserImage +  "%");
+
+                        }
+                    })
+                     .addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                         @Override
+                         public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                             if ( task.isSuccessful())
+                             {
+                                String downloadUrl;
+                                downloadUrl = task.getResult().getStorage().getDownloadUrl().toString();
+                                getUserDataReference.child("user_image").setValue(downloadUrl);
+
+                             }
+                         }
+                     });
+
+        }
+
     }
 }
